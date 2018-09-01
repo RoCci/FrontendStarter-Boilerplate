056c49a5b75b3d22c0ede3cdce034e993f08e031
Riccardo Krone <riccardo.krone@gmail.com>
2018-09-01 07:59:02 +0200

    added firebase auth (test version)

--- src/frontend/src/static/assets/js/modules/navigationActiveClass.js
+++ src/frontend/src/static/assets/js/modules/navigationActiveClass.js
@@ -13,7 +13,7 @@ class NavigationActiveClass {
         $('.primary-nav li a').each(function () { //looks in each link item within the primary-nav list
             var linkPage = this.href.substring(this.href.lastIndexOf('/') + 1); //sets the variable "linkPage" as the substring of the url path in each &lt;a&gt;
 
-            if (activePage == linkPage) { //compares the path of the current window to the path of the linked page in the nav item
+            if (activePage === linkPage) { //compares the path of the current window to the path of the linked page in the nav item
                 $(this).parent().addClass('active'); //if the above is true, add the "active" class to the parent of the &lt;a&gt; which is the &lt;li&gt; in the nav list
             }
         });
